ed1ac814fa9ab8ef3a7fd8e634b49d18e553a0fa
Andrew Caniglia <andrewcaniglia@gmail.com>
2020-12-29 06:44:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+katz_deli = []
+def line(array)
+  if array.length() > 0
+    i = 1
+    list = []
+    while i <= array.length
+    str = "The line is currently"
+    array.each do |customer|
+      list <<"#{i}. #{customer}"
+      i +=1
+    end
+  end
+end
+    #puts 'The line is currently empty.'
+
+
+def take_a_number(array, name)
+  puts "Welcome, #{name}. You are number #{array.length()+1} in line."
+  katz_deli << "#{name}"
+end
+
+def now_serving
+  if katz_deli.length()>0
+    katz_deli.shift
+  else
+    "There is nobody waiting to be served!"
+  end
+end
