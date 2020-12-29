2474f231b8df75ca1020135acaa22548415816f7
Andrew Caniglia <andrewcaniglia@gmail.com>
2020-12-29 07:43:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,7 @@ def line(array)
       i +=1
     end
     list.each do |consumer|
-      puts "The line is currently: #{list}"
+      puts "The line is currently: #{list.}"
   else
     puts "The line is currently empty."
   end
